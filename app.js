ab56319de30a63af510bb8ecd6f78c1e609ff41a
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2025-05-14 16:35:44 -0300

    Revert "primeiro teste de ZOD"
    This reverts commit 9ab96c6877d18288b56bc9e16ede6c3887103a77.

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const createHttpError = require('http-errors'); // Importando aqui
 
 // Importando as rotas
 const feedRoutes = require('./routes/feedRoutes');
-const commentRoutes = require('./routes/commentRoutes');
+const commentRoutes = require('./routes/commentRoutes')
 const authRoutes = require('./routes/authRoutes');
 const perfilRoutes = require('./routes/perfilRoutes');
+const relatorioRoutes = require('./routes/relatorioRoutes')
+const imageRoutes = require('./routes/imageRoutes');
+const fixProblemRoutes = require('./routes/fixProblemRoutes');
 
-// Middlewares
+
+// Importando o middlewares
 const logger = require('./middlewares/logger');
 const errorHandler = require('./utils/errorHandler');
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 const app = express();
 
-// Configuração do CORS
+// Cors - config
 const corsOptions = {
   origin: process.env.CLIENT_ORIGIN_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 };
 
+// Remova o cors config apenas em desenv
 app.use(cors(corsOptions));
 app.use(logger);
 app.use(express.json());
@@ -32,18 +36,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Rotas
 app.use('/auth', authRoutes);
-app.use('/feed', feedRoutes);
-app.use('/comments', commentRoutes);
+app.use('/relatorios', relatorioRoutes);
 app.use('/user', perfilRoutes);
+app.use('/feed', feedRoutes);
+app.use('/image', imageRoutes);
+app.use('/fix-problem', fixProblemRoutes);
+app.use('/comments', commentRoutes);
 
-// Middleware para tratamento de erros
-app.use((err, req, res, next) => {
-  console.error(err); // Para logar o erro
-  const status = err.status || 500;  // Se não tiver status, utiliza 500
-  res.status(status).json({ message: err.message || 'Erro desconhecido' });
-});
+// app.use('/', perfilRoutes, commentRoutes, feedRoutes);
+
+// Tratamento de erros
+app.use(errorHandler);
 
 module.exports = app;
